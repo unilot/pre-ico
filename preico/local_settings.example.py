5fa124751a392c62ec1e4da48a86f9df01b2d4ac
Alegzander <alexander.aka.alegz@gmail.com>
2017-12-09 04:06:28 +0600

    Configured settings file. Setted up local settings management.

---
+++ preico/local_settings.example.py
@@ -0,0 +1,32 @@
+import os
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = None #Setup very secure very secret key
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'your-pg-db-name',
+        'USER': 'yout-pg-username',
+        'PASSWORD': 'your-pg-password',
+        'HOST': 'your-pg-db-host',
+        # 'PORT': 'your-pg-db-port', #If required
+    }
+}
+
+SESSION_REDIS = {
+    'host': None,
+    'unix_domain_socket_path': '/var/run/redis/redis.sock',
+    'db': 15,
+    'password': 'password',
+    'prefix': 'unilot-pre-ico-session',
+    'socket_timeout': 1
+}
