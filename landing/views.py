8c010816774ea293fbe5f43aa60a17ed19a9733a
Alegzander <alexander.aka.alegz@gmail.com>
2017-12-20 07:00:53 +0600

    Added live faq.

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, response
+from cp import models as cp_models
 
 
 class LandingView(generics.GenericAPIView):
@@ -6,4 +7,10 @@ class LandingView(generics.GenericAPIView):
     template_name = 'landing.html'
 
     def get(self, request, *args, **kwargs):
-        return response.Response()
+        faqs_qs = cp_models.FAQ.objects\
+            .filter(published=True).order_by('-id')
+
+        faqs_total = faqs_qs.count()
+        faqs = faqs_qs[:12]
+
+        return response.Response({'faqs': faqs, 'faqs_total': faqs_total})
