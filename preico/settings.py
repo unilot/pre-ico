a2741e79bc7f20a0eb95afd1f1835d2a49e03e84
Alegzander <alexander.aka.alegz@gmail.com>
2018-01-03 20:41:33 +0600

    Updated terms and conditions and affiliate terms and conditions documents. Added humanize app to project. Added configurable titles for team and advisors. Updated css with fixes.

--- preico/settings.py
+++ preico/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'cp.app.BackendConfig',
     'console.app.BackendConfig',
     'landing.apps.LandingConfig',
