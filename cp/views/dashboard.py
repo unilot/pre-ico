e02ea79ebd473e34f6812e3694392ea56021e12d
Alegzander <alexander.aka.alegz@gmail.com>
2018-01-04 23:19:50 +0600

    Added Text model with migration. Replaced static text in dashboard and buy token modal with dynamic tag. Confgured Text model to display in django admin panel.

--- cp/views/dashboard.py
+++ cp/views/dashboard.py
@@ -1,5 +1,8 @@
 from rest_framework import views, response, permissions
 
+from cp.templatetags.dynamic_content import content_text
+from .. import models
+
 
 class DashboardView(views.APIView):
     permission_classes = [ permissions.IsAuthenticated ]
